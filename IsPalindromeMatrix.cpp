0fd0fc2d91dd2e74310b095b9bd0fb2f2825d07f
Omar jaghoob <progomar03@gmail.com>
2024-10-23 15:27:07 +0300

    Create IsPalindromeMatrix.cpp

---
+++ IsPalindromeMatrix.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+int RandomNumber(int From,int To) {
+    int randNum = rand()%(To-From+1)+From;
+    return randNum;
+}
+void FillMatrixWithRandomNumbers(int arr[3][3],short Rows,short Cols) {
+    for (int i = 0;i<Rows;i++) {
+        for (int j = 0;j<Cols;j++) {
+            arr[i][j]=RandomNumber(1,10);
+        }
+    }
+}
+void PrintMatrix(int arr[3][3],short Rows,short Cols ) {
+    for (int i = 0;i<Rows;i++) {
+        for (int j = 0;j<Cols;j++) {
+            printf(" %0*d ", 2, arr[i][j]);
+        }
+        cout << '\n';
+    }
+}
+int IsPalindromeMatrix(int Matrix[3][3],short Rows,short Cols) {
+    for (short i = 0;i<Rows;i++) {
+        for (short j = 0;j<Cols/2;j++) {
+            if(Matrix[i][j]!=Matrix[i][Cols-1-j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+int main()
+{
+    srand((unsigned) time(NULL));
+    int Matrix1[3][3]={{997,5,12},{22,20,1},{1,7,9}};
+    //int Matrix2[3][3]={{5,80,90},{22,77,1},{10,8,33}};
+    cout << "\nMatrix1\n";
+    PrintMatrix(Matrix1,3,3);cout << endl;
+    
+
+}
